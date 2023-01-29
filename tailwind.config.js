cffd2c0a64973cfb01c4753b6128db563b798c1c
Anis Kehila <aniskehila22@gmail.com>
2023-01-29 12:19:25 +0100

    modify links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","src/main.js"],
+  content: ["./index.html","./src/main.js"],
   theme: {
     extend: {},
   },
